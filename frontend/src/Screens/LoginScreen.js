edb29e176fa144781aa511ff2292df7ecb0c5e2f
Rizaldo Abdulrachman <rizaldo.a.r@gmail.com>
2021-05-24 21:55:50 +0700

    login api integrated

--- frontend/src/Screens/LoginScreen.js
+++ frontend/src/Screens/LoginScreen.js
@@ -1,12 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState  } from "react";
+import { Link, useHistory } from "react-router-dom";
 import data from "../data";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Input, FormGroup, Label, Button } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
+import { setUserSession } from "../utils/common";
+
+
+
 
 function LoginScreen(props) {
+  
+  
+  const [email, setEmail] =useState('');
+  const [password, setPassword] =useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+ 
+
+
+  function login(){
+    setError(null);
+    setLoading(true);
+    axios.post("http://localhost:5000/api/login", {
+      username: email,
+      password: password
+    }).then(response =>{
+      setLoading(false);
+      setUserSession(response.data.token, response.data.user);
+      if(response.data.isAdmin){
+        props.history.push('/admin');
+      }
+      else{
+        
+         props.history.push('/');
+      }
+      
+    }).catch(error =>{
+      setLoading(false);
+      if (error.response.status === 401) setError(error.response.data.message);
+      else setError("Something went wrong. Please try again later.")
+    });
+  }
+
+
   return (
     <div className="aboutBody">
       <div className="container my-5">
@@ -20,6 +60,7 @@ function LoginScreen(props) {
                 name="email"
                 id="exampleEmail"
                 placeholder="with a placeholder"
+                onChange={(e)=>setEmail(e.target.value)}
               />
             </FormGroup>
             <FormGroup className="mb-5">
@@ -29,10 +70,12 @@ function LoginScreen(props) {
                 name="password"
                 id="examplePassword"
                 placeholder="password placeholder"
+                onChange={(e)=>setPassword(e.target.value)}
+                
               />
             </FormGroup>
             <FormGroup inline>
-              <Link to="/saveSession"><Button color="success">Login</Button></Link>{" "}
+              <Button onClick={login} color="success">Login</Button>
               <Link to="/register"><Button color="danger">Register</Button></Link>
             </FormGroup>
           </Form>
